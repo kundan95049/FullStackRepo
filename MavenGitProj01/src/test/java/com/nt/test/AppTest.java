b2c4c9cad4602af003e66a1ee7c61eac4c3e235d
KUNDAN KUMAR <KUNDAN KUMAR@Kundan>
2024-07-14 00:38:59 +0530

    tl-new test with negative

--- MavenGitProj01/src/test/java/com/nt/test/AppTest.java
+++ MavenGitProj01/src/test/java/com/nt/test/AppTest.java
@@ -1,15 +1,28 @@
 package com.nt.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-
-
+import org.junit.jupiter.api.Test;
 
 public class AppTest 
 {
+	@Test
   public void teatWithPositive()
   {
 	  Arithmetic ar=new Arithmetic();
+	  int actual=30;
+	  int expected=ar.sum(10,20);
+	  assertEquals(actual,expected);
+	  
   }
+	@Test
+	  public void teatWithNegative()
+	  {
+		  Arithmetic ar=new Arithmetic();
+		  int actual=-30;
+		  int expected=ar.sum(-10,-20);
+		  assertEquals(actual,expected);
+		  
+	  }
   
 }
